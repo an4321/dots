6bee601c95d75cf785232e666d6128e8178ddf8f
Anurag <anuragp.584@gmail.com>
2024-02-05 13:20:11 +0530

    fixed tmux script

--- .config/fish/tmux.fish
+++ .config/fish/tmux.fish
@@ -1,19 +1,16 @@
 
-function t --description "jump to Tmux"
-    set dir1 (zoxide query $argv[1])
-
-    # Check if dir1 is equal to a single dot (.)
-    if test "$dir1" = "."
-        return
-    end
-
-    set sessions (tmux ls)
-
-    if set -q TMUX
-        tmux new -ds $(basename $dir1) -c $dir1
-        tmux switch-client -t $(basename $dir1)
+function t --description "Jump to Tmux"
+    set target_dir (zoxide query $argv[1])
+    
+    if test -d "$target_dir"
+        if set -q TMUX
+            tmux new -ds (basename $target_dir) -c $target_dir
+            tmux switch-client -t (basename $target_dir)
+        else
+            tmux new -s (basename $target_dir) -c $target_dir
+        end
     else
-        tmux new -s $(basename $dir1) -c $dir1
+        echo "Error: Not a valid directory."
     end
 end
 
