974baf31b95c674447907ef80e372357445100f8
Anurag <anuragp.584@gmail.com>
2024-04-11 22:55:59 +0530

    prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -4,10 +4,26 @@ function fish_prompt
     end
 
     echo
-    echo -n (set_color -o blue)(prompt_pwd)' '
+    echo -n (set_color -o purple)(prompt_pwd)' '
+
+    echo -n (set_color green)'󰁕 '
+    set_color normal
+end
+
+function fish_prompt_one
+    if test -n "$SSH_TTY"
+        echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
+    end
+
+    echo
+    echo -n (set_color -o 55B6C2)(prompt_pwd)' '
+
+    if test "$PWD" != "$HOME"
+        echo
+    end
 
     # echo -n (set_color red)''(set_color yellow)''(set_color green)' '
-    # echo -n (set_color 4efce0)' '
+    # echo -n (set_color 3ac7b0)' '
     echo -n (set_color 98C379)' '
     set_color normal
 end
