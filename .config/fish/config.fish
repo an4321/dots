9d43d14f061e255c0c5d94428c3924190cf5196e
Anurag <anuragp.5984@gmail.com>
2024-07-22 16:31:03 +0530

    .

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,7 +14,7 @@ bind H beginning-of-line
 bind L end-of-line
 
 bind -M insert \eq 'exit'
-bind -M insert \ef '$FM; commandline -f repaint'
+bind -M insert \ef 'fm; commandline -f repaint'
 bind -M insert \ec 'cd .; clear; commandline -f repaint'
 bind -M insert \ea 'tmux_attach; commandline -f repaint'
 bind -M insert \ek 'commandline -i $(history | fzf-tmux -h +s); commandline -f repaint'
