8e12f2a002b0e3d3a7b13e06d1cbdfd28db4feac
Anurag <anuragp.5984@gmail.com>
2024-09-13 23:31:47 +0530

    Cleanup

--- .config/nvim/lua/plugins/basic.lua
+++ .config/nvim/lua/plugins/basic.lua
@@ -27,7 +27,9 @@ return {
     {
         "folke/flash.nvim",
         event = "VeryLazy",
-        opts = {},
+        opts = {
+            jump = { autojump = true, },
+        },
         keys = {
             { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
             { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
