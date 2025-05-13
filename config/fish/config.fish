f57421fd4ce316346b3b491995c5bb5602a7979f
anurag <anuragp.5984@gmail.com>
2025-05-13 23:12:28 +0530

    integrates tmux-attach to t menu and adds case for home

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -13,8 +13,7 @@ bind L end-of-line
 
 bind -M insert \eq 'exit'
 bind -M insert \ef 'fm; commandline -f repaint'
-bind -M insert \ea 'tmux-attach; commandline -f repaint'
-bind -M insert \eA 't $PWD; commandline -f repaint'
+bind -M insert \ea 't menu; commandline -f repaint'
 bind -M insert \ck 'commandline -i $(history | fzf-tmux -h -e +s); commandline -f repaint'
 bind -M insert \cH backward-kill-word
 
