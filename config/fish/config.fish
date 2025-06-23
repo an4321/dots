fab22331079dbc336d66d58e5fa699b0205a002a
anurag <anuragp.5984@gmail.com>
2025-06-23 16:58:39 +0530

    switch to sj from zoxide

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,13 +1,11 @@
 set -gx fish_greeting
-fish_hybrid_key_bindings
+set -gx fish_hybrid_key_bindings
 set -gx fish_cursor_insert line
-
-zoxide init fish --cmd j | source
 
 source ~/.config/alias.sh
 source ~/.config/fish/functions.fish
+source ~/dots/bin/sj-init.fish
 
-bind q 'exit'
 bind H beginning-of-line
 bind L end-of-line
 
@@ -16,7 +14,6 @@ bind -M insert \ef 'fm; commandline -f repaint'
 bind -M insert \ea 't menu; commandline -f repaint'
 bind -M insert \ck 'commandline -i $(history | fzf-tmux -h -e +s); commandline -f repaint'
 bind -M insert \cH backward-kill-word
-
 
 if test (tty) = "/dev/tty1"
     pgrep dwm || startx
