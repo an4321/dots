a1af62848b8337471df4afb9a202968ef0cc8ca4
anurag <anuragp.5984@gmail.com>
2025-10-06 00:09:10 +0530

    better way to do options in vim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,33 +1,17 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.confirm = true
-vim.opt.scrolloff = 10
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.inccommand = 'split'
-vim.opt.signcolumn = 'yes'
-vim.opt.list = true
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undofile = true
-vim.opt.history = 10000
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.wrap = false
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-vim.opt.winborder = 'rounded'
+for k, v in pairs({
+	number, relativenumber, confirm, ignorecase, smartcase, list,
+	undofile, autoindent, smartindent, splitright, splitbelow,
+	swapfile = false, backup = false, wrap = false, scrolloff = 15,
+	history = 10000, tabstop = 4, shiftwidth = 4, cmdheight = 0, laststatus = 3,
+	inccommand = "split", signcolumn = 'yes', winborder = 'rounded',
+}) do vim.opt[k] = v and v or true end
+
 vim.schedule(function() vim.opt.clipboard = 'unnamedplus' end)
 vim.opt.nrformats = vim.opt.nrformats + { 'unsigned' }
 
 vim.keymap.set('n', '<esc>', ':nohlsearch<cr>')
 vim.keymap.set('n', 'U', ':redo<cr>')
 vim.keymap.set({ 'n', 'v' }, ';', ':')
-vim.keymap.set({ 'n', 'v' }, ',', 'zt')
-vim.keymap.set({ 'n', 'v' }, 'j', 'gj')
-vim.keymap.set({ 'n', 'v' }, 'k', 'gk')
 vim.keymap.set({ 'n', 'v' }, 'L', '$')
 vim.keymap.set({ 'n', 'v' }, 'H', '^')
 vim.keymap.set({ 'i', 'c' }, '<c-bs>', '<c-w>', { noremap = true })
@@ -35,7 +19,7 @@ vim.keymap.set({ 'i', 'c' }, '<c-h>', '<c-w>', { noremap = true })
 vim.keymap.set('v', 'gp', '"_dP')
 vim.keymap.set('v', '<tab>', '>gv', { noremap = true, silent = true })
 vim.keymap.set('v', '<s-tab>', '<gv', { noremap = true, silent = true })
-vim.keymap.set('n', '<space>q', ':q<cr>')
+vim.keymap.set('n', '<space>q', ':quit<cr>')
 vim.keymap.set('n', '<space>w', ':update<cr>')
 vim.keymap.set('n', '<space>x', ':bdelete<cr>')
 vim.keymap.set('n', '<space>l', ':bnext<cr>')
@@ -57,8 +41,6 @@ vim.cmd [[ au BufReadPost * if line("'\"") >= 1 && line("'\"") <= line("$") | ex
 vim.cmd [[ autocmd TermOpen * startinsert ]]
 
 -- statusline
-vim.opt.cmdheight = 0
-vim.opt.laststatus = 3 -- global statusline
 require('vim._extui').enable({}) -- temp
 function recording_status()
 	return vim.fn.reg_recording() ~= '' and 'recording @' .. vim.fn.reg_recording() or ''
@@ -73,7 +55,6 @@ vim.pack.add({
 	'https://github.com/folke/flash.nvim',
 	'https://github.com/ibhagwan/fzf-lua',
 	'https://github.com/lewis6991/gitsigns.nvim',
-
 	'https://github.com/neovim/nvim-lspconfig',
 	'https://github.com/mason-org/mason.nvim',
 	'https://github.com/mason-org/mason-lspconfig.nvim',
@@ -84,6 +65,7 @@ vim.pack.add({
 require('catppuccin').setup({
 	transparent_background = true, float = { transparent = true }
 })
+vim.cmd.colorscheme 'catppuccin-mocha'
 vim.schedule(function()
 	require('oil').setup({
 		skip_confirm_for_simple_edits = true, delete_to_trash = true,
@@ -102,7 +84,6 @@ vim.schedule(function()
 	vim.lsp.config('*', { capabilities = capabilities })
 end)
 
-vim.cmd.colorscheme 'catppuccin-mocha'
 vim.keymap.set('n', '<space>e', ':Oil<cr>')
 vim.keymap.set('n', 's', function() require('flash').jump() end)
 vim.keymap.set('n', 'S', function() require('flash').treesitter() end)
