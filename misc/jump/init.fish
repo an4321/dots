94c9e266723848bdae08858098c72df9156a2bff
anurag <anuragp.5984@gmail.com>
2025-12-19 15:04:01 +0530

    things

--- misc/jump/init.fish
+++ misc/jump/init.fish
@@ -2,12 +2,11 @@ function j --wraps="cd"
 	set -l argc (count $argv)
 	if test $argc -eq 0
 		cd "$HOME/Desktop"
-	else if test "$argv" = -
-		cd -
 	else if test $argc -eq 1 -a -d $argv[1]
 		cd $argv[1]
 	else
-		cd "$(jump query $argv || return 1)" 2>/dev/null
+		set target (jump query $argv || return 1)
+		cd "$target" 2>/dev/null
 	end
 	jump add $PWD >/dev/null
 end
--- misc/jump/init.sh
+++ misc/jump/init.sh
@@ -1,12 +1,11 @@
 j() {
 	if [ $# -eq 0 ]; then
 		cd ~/Desktop
-	elif [ "$1" = "-" ]; then
-		cd -
 	elif [ $# -eq 1 ] && [ -d "$1" ]; then
 		cd "$1"
 	else
-		cd "$(jump query "$@" || return 1)" 2>/dev/null
+		target=$(jump query "$@" || return 1)
+		cd "$target" 2>/dev/null
 	fi
 	jump add "$PWD" >/dev/null
 }
