be803d68f032ca2d649cf564af800f1e9b834c3e
anurag <anuragp.5984@gmail.com>
2025-12-24 23:29:34 +0530

    some reformatting and switch to mini-files.nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,52 +1,29 @@
-for k, v in pairs({
-	number = true, relativenumber = true, confirm = true, list = true,
-	wrap = false, ignorecase = true, smartcase = true, undofile = true,
-	autoindent = true, smartindent = true, splitright = true, splitbelow = true,
-	swapfile = false, backup = false, scrolloff = 15, history = 10000,
-	tabstop = 4, shiftwidth = 4, cmdheight = 0, laststatus = 3,
-	inccommand = "split", signcolumn = "yes", winborder = "rounded",
-}) do vim.opt[k] = v end
-
-require('vim._extui').enable({})
-vim.schedule(function() vim.opt.clipboard = 'unnamedplus' end)
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.confirm = true
+vim.opt.list = true
+vim.opt.wrap = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.undofile = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.scrolloff = 15
+vim.opt.history = 10000
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.cmdheight = 0
+vim.opt.laststatus = 3
+vim.opt.inccommand = "split"
+vim.opt.signcolumn = "yes"
+vim.opt.winborder = "rounded"
 vim.opt.nrformats = vim.opt.nrformats + { 'unsigned' }
-
-vim.keymap.set({'n', 'v'}, '<c-s>', '<cmd>update<cr>')
-vim.keymap.set({'n', 'v'}, '<c-q>', function()
-	vim.cmd(#vim.fn.getbufinfo({ buflisted = 1 }) > 1 and 'bd' or 'q')
-end)
-vim.keymap.set('n', '<esc>', '<cmd>nohls<cr>')
-vim.keymap.set('n', 'U', '<cmd>redo<cr>')
-vim.keymap.set({ 'n', 'v' }, ';', ':')
-vim.keymap.set({ 'n', 'v' }, 'L', '$')
-vim.keymap.set({ 'n', 'v' }, 'H', '^')
-vim.keymap.set({ 'i', 'c' }, '<c-bs>', '<c-w>', { noremap = true })
-vim.keymap.set({ 'i', 'c' }, '<c-h>', '<c-w>', { noremap = true })
-vim.keymap.set('v', 'gp', '"_dP')
-vim.keymap.set('v', '<tab>', '>gv', { noremap = true, silent = true })
-vim.keymap.set('v', '<s-tab>', '<gv', { noremap = true, silent = true })
-vim.keymap.set('n', '<space>h', '<cmd>bp<cr>')
-vim.keymap.set('n', '<space>l', '<cmd>bn<cr>')
-vim.keymap.set('n', '<space>n', '<cmd>enew<cr>')
-vim.keymap.set('n', '<space>j', '<c-w><c-w>')
-vim.keymap.set('n', '<space>w', '<cmd>set wrap!<cr>')
-vim.keymap.set('n', '<space>s', '<cmd>set spell!<cr>')
-vim.keymap.set('n', '<space>r', [[:%s/\<<c-r><c-w>\>/<c-r><c-w>/gI<Left><Left><Left>]])
-for _, i in ipairs({ '()', '[]', '{}', '""', '``' }) do
-	vim.keymap.set('i', i:sub(1,1), i .. '<left>')
-end
-
-vim.keymap.set('n', 'S', '1z=')
-vim.keymap.set('n', '+', '<c-a>')
-vim.keymap.set('n', '-', '<c-x>')
-vim.keymap.set('v', '+', 'g<c-a>')
-vim.keymap.set('v', '-', 'g<c-x>')
-vim.keymap.set('n', '?', ':%s/')
-vim.keymap.set('n', '<bs>', ':term ')
-vim.keymap.set('t', '<c-c>', [[<c-\><c-n>]])
-vim.keymap.set('n', 'j', 'gj')
-vim.keymap.set('n', 'k', 'gk')
-vim.keymap.set('n', ':', ';')
+vim.schedule(function() vim.opt.clipboard = 'unnamedplus' end)
+require('vim._extui').enable({})
 
 -- highlight on yank
 vim.cmd [[ au TextYankPost * lua vim.hl.on_yank() ]]
@@ -64,7 +41,7 @@ vim.opt.statusline = '%#StatusLine#  %f %h%w%m%r %{%v:lua.recording_status()%} %
 -- packages
 vim.pack.add({
 	'https://github.com/catppuccin/nvim',
-	'https://github.com/stevearc/oil.nvim',
+	'https://github.com/nvim-mini/mini.files',
 	'https://github.com/ibhagwan/fzf-lua',
 	'https://github.com/lewis6991/gitsigns.nvim',
 	'https://github.com/neovim/nvim-lspconfig',
@@ -74,7 +51,7 @@ vim.pack.add({
 	'https://github.com/saghen/blink.cmp',
 })
 
-require('oil').setup({ skip_confirm_for_simple_edits = true, delete_to_trash = true })
+require('mini.files').setup({ options = { permanent_delete = false } })
 require('catppuccin').setup({ transparent_background = true, float = { transparent = true } })
 vim.cmd.colorscheme 'catppuccin-mocha'
 
@@ -87,7 +64,7 @@ vim.schedule(function()
 	vim.lsp.config('*', { capabilities = require('blink.cmp').get_lsp_capabilities() })
 end)
 
-vim.keymap.set('n', '<space>f', '<cmd>Oil<cr>')
+vim.keymap.set('n', '<space>f', '<cmd>lua MiniFiles.open()<cr>')
 vim.keymap.set('n', '<space>a', '<cmd>FzfLua files<cr>')
 vim.keymap.set('n', '<space>g', '<cmd>FzfLua live_grep<cr>')
 vim.keymap.set('n', '<space>c', '<cmd>cd ~/dots | FzfLua files<cr>')
@@ -97,3 +74,37 @@ vim.keymap.set('n', ']g', '<cmd>Gitsigns next_hunk<cr>')
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
 vim.keymap.set('n', 'gq', function() vim.lsp.buf.format({ async = true }) end)
 vim.keymap.set('n', '<space>d', function() vim.diagnostic.jump({count = 1, float = true}) end)
+
+for _, i in ipairs({ '()', '[]', '{}', '""', '``' }) do
+	vim.keymap.set('i', i:sub(1,1), i .. '<left>')
+end
+
+vim.keymap.set({'n', 'v'}, '<c-q>', function()
+	vim.cmd(#vim.fn.getbufinfo({ buflisted = 1 }) > 1 and 'bd' or 'q')
+end)
+vim.keymap.set({'n', 'v'}, '<c-s>', '<cmd>update<cr>')
+
+vim.keymap.set('n', '<c-h>', '<cmd>bp<cr>')
+vim.keymap.set('n', '<c-l>', '<cmd>bn<cr>')
+vim.keymap.set('n', '<esc>', '<cmd>nohls<cr>')
+vim.keymap.set('n', 'U', '<cmd>redo<cr>')
+vim.keymap.set({ 'n', 'v' }, ';', ':')
+vim.keymap.set('n', 's', ';')
+vim.keymap.set({ 'n', 'v' }, 'L', '$')
+vim.keymap.set({ 'n', 'v' }, 'H', '^')
+vim.keymap.set({ 'i', 'c' }, '<c-bs>', '<c-w>')
+vim.keymap.set({ 'i', 'c' }, '<c-h>', '<c-w>')
+vim.keymap.set('v', 'gp', '"_dP')
+vim.keymap.set('v', '<tab>', '>gv')
+vim.keymap.set('v', '<s-tab>', '<gv')
+vim.keymap.set('n', 'S', '1z=')
+vim.keymap.set('n', '+', '<c-a>')
+vim.keymap.set('n', '-', '<c-x>')
+vim.keymap.set('v', '+', 'g<c-a>')
+vim.keymap.set('v', '-', 'g<c-x>')
+vim.keymap.set('n', 'j', 'gj')
+vim.keymap.set('n', 'k', 'gk')
+vim.keymap.set('n', '?', ':%s/')
+vim.keymap.set('n', '<space>r', [[:%s/\<<c-r><c-w>\>/<c-r><c-w>/gI<Left><Left><Left>]])
+vim.keymap.set('n', '<bs>', ':term ')
+vim.keymap.set('t', '<c-c>', [[<c-\><c-n>]])
